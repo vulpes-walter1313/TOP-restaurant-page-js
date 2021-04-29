94af4018425c29a64f306edbba912e6973702e39
Walter Fritsche <wfritsche95@protonmail.com>
2021-04-29 17:22:10 -0500

    added css loaders, host 0.0.0.0, and a non-working asset path

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 let mode = "development";
 
 if (process.env.NODE_ENV === "production") {
@@ -8,8 +10,26 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   mode: mode,
   entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: "img/[hash][ext]",
+  },
   module: {
     rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.scss$/,
+        use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: { publicPath: ""},
+        }, "css-loader",
+            "sass-loader"]
+      },
       {
       test: /\.js$/,
       exclude: /node_modules/,
@@ -19,6 +39,7 @@ module.exports = {
     },
   ]
   },
+  plugins: [new MiniCssExtractPlugin()],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -26,5 +47,7 @@ module.exports = {
   devtool: "source-map",
   devServer: {
     contentBase: path.resolve(__dirname, "dist"),
+    hot: true,
+    host: '0.0.0.0',
   }
 };
