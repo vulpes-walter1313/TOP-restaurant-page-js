789f9720606f9e822be4ac6828af8f0352c3ba0d
Walter Fritsche <wfritsche95@protonmail.com>
2021-04-29 17:33:55 -0500

    init commit for contactTab module

---
+++ src/js/contactTab.js
@@ -0,0 +1,14 @@
+function contactTab(section) {
+    section.innerHTML = `<h2>Contact Us</h2><form action="" method="get" class="contact-form">
+    <label for="name-in">Enter your name</label>
+    <input type="text" name="name" id="name-in">
+    <label for="email-in">Enter your email</label>
+    <input type="text" name="email" id="email-in">
+    <textarea id="story" name="story">I would like to tell you...
+    </textarea>
+    <button>Submit</button>
+  </form>`;
+  return section;
+}
+
+export default contactTab;
